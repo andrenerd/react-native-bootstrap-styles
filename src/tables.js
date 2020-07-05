f56a694ed880547d77d5ea0dd30620bf8735515e
Andrenerd <andreid.31415@gmail.com>
2020-07-05 22:01:08 +0200

    prototype tables

---
+++ src/tables.js
@@ -0,0 +1,50 @@
+import { mixinButtonSize, mixinButtonVariant, mixinButtonOutlineVariant } from './mixins/buttons';
+
+export default function getClasses(constants, classes) {
+  const {
+    SPACER,
+    TABLE_COLOR,
+    TABLE_BG,
+    TABLE_BORDER_WIDTH,
+    TABLE_BORDER_COLOR,
+    TABLE_CELL_PADDING,
+    TABLE_CELL_PADDING_SM,
+  } = constants;
+
+  const _classes = {
+
+    table: {
+      width: '100%',
+      marginBottom: SPACER,
+      color: TABLE_COLOR, // TODO: check
+      backgroundColor: TABLE_BG,
+    },
+
+    tableTh: {
+      padding: TABLE_CELL_PADDING,
+      justifyContent: 'flex-start',
+      borderTopWidth: TABLE_BORDER_WIDTH,
+      borderTopColor: TABLE_BORDER_COLOR,
+    },
+
+    tableTd: {
+      padding: TABLE_CELL_PADDING,
+      justifyContent: 'flex-start',
+      borderTopWidth: TABLE_BORDER_WIDTH,
+      borderTopColor: TABLE_BORDER_COLOR,
+    },
+
+    // table: {
+    //   // thead th {
+    //   //   vertical-align: bottom;
+    //   //   border-bottom: (2 * $table-border-width) solid $table-border-color;
+    //   // }
+
+    //   // tbody + tbody {
+    //   //   border-top: (2 * $table-border-width) solid $table-border-color;
+    //   // }
+    // }
+  };
+
+  return _classes;
+};
