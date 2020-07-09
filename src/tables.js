7237f6e9fbd0776ecca5e93409fb987b73b7d32a
Andrenerd <andreid.31415@gmail.com>
2020-07-09 02:23:58 +0200

    upgrade tables

--- src/tables.js
+++ src/tables.js
@@ -59,15 +59,39 @@ export default function getClasses(constants, classes) {
     tableTbodyTr: {
       flexDirection: 'row',
     },
+
+    // Border versions
+    tableBordered: {
+      borderStyle: 'solid',
+      borderTopWidth: TABLE_BORDER_WIDTH,
+      borderTopColor: TABLE_BORDER_COLOR,
+      borderLeftWidth: TABLE_BORDER_WIDTH,
+      borderLeftColor: TABLE_BORDER_COLOR,
+    },
+
+    tableBorderedTbodyTd: {
+      borderStyle: 'solid',
+      borderBottomWidth: TABLE_BORDER_WIDTH,
+      borderBottomColor: TABLE_BORDER_COLOR,
+      borderRightWidth: TABLE_BORDER_WIDTH,
+      borderRightColor: TABLE_BORDER_COLOR,
+    },
+
+    tableBorderedTheadTh: {
+      borderStyle: 'solid',
+      borderBottomWidth: 2 * TABLE_BORDER_WIDTH,
+      borderRightWidth: TABLE_BORDER_WIDTH,
+      borderRightColor: TABLE_BORDER_COLOR,
+    },
   };
 
-  // lets do full copy
-  tableSmTbodyTd = Object.assign({}, tableTbodyTd, {
+  // lets make full copy
+  tableSmTbodyTd = Object.assign({}, _classes.tableTbodyTd, {
     padding: TABLE_CELL_PADDING_SM,
   });
 
-  // lets do full copy
-  tableSmTheadTh = Object.assign({}, tableTheadTh, {
+  // lets make full copy
+  tableSmTheadTh = Object.assign({}, _classes.tableTheadTh, {
     padding: TABLE_CELL_PADDING_SM,
   });
 
@@ -75,15 +99,21 @@ export default function getClasses(constants, classes) {
   _classes.tableTh = _classes.tableTheadTh;
   _classes.tableTr = _classes.tableTbodyTr;
   _classes.tableTd = _classes.tableTbodyTd;
+  _classes.tableBorderedTh = _classes.tableBorderedTheadTh;
+  _classes.tableBorderedTd = _classes.tableBorderedTbodyTd;
 
   // aliases
   _classes.tableHead = _classes.tableThead;
   _classes.tableHeadCol = _classes.tableTheadTh;
   _classes.tableSmHeadCol = _classes.tableSmTheadTh;
+  _classes.tableBorderedHeadCol = _classes.tableBorderedTheadTh;
+
   _classes.tableBody = _classes.tableTbody;
   _classes.tableRow = _classes.tableTbodyTr;
   _classes.tableCol = _classes.tableTbodyTd;
+
   _classes.tableSmCol = _classes.tableSmTbodyTd;
+  _classes.tableBorderedCol = _classes.tableBorderedTbodyTd;
 
   return _classes;
 };
