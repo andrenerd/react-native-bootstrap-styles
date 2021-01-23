d8d968e0b8acfb065ec5c68f0a5176b6fd1280e9
Andrenerd <andreid.31415@gmail.com>
2021-01-23 23:54:56 +0100

    fix mono font for android

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // TODO: rename the file to "variables", keeping backward compatibility
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, Platform } from 'react-native';
 import Color from 'color';
 
 export default function getConstants(constants) {
@@ -205,9 +205,8 @@ export default function getConstants(constants) {
     COMPONENT_ACTIVE_BG = _('COMPONENT_ACTIVE_BG', PRIMARY),
 
     // Fonts
-
     FONT_FAMILY_SANS_SERIF = _('FONT_FAMILY_SANS_SERIF', 'System'), // iOS: 'San Francisco'
-    FONT_FAMILY_MONOSPACE = _('FONT_FAMILY_MONOSPACE', 'Courier New'), // iOS: 'Courier New',
+    FONT_FAMILY_MONOSPACE = _('FONT_FAMILY_MONOSPACE', Platform.OS === 'ios' ? 'Courier New' : 'monospace'), // ios: 'Courier New' //  android: 'monospace'
     FONT_FAMILY_BASE = _('FONT_FAMILY_BASE', FONT_FAMILY_SANS_SERIF),
     FONT_FAMILY_BASE_LIGHT = _('FONT_FAMILY_BASE_LIGHT', FONT_FAMILY_SANS_SERIF),
     FONT_FAMILY_BASE_BOLD = _('FONT_FAMILY_BASE_BOLD', FONT_FAMILY_SANS_SERIF),
